ace68ddc069bc475b5e898d88dc9d1b716fdc6b2
f6o <25875630+f6o@users.noreply.github.com>
2024-12-22 14:21:35 +0900

    move to server context

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,1 @@
 // place files you want to import through the `$lib` alias in this folder.
-
-import firebase from 'firebase/app'
-import { getDatabase } from 'firebase/database'
-
-const databaseURL = process.env.FIREBASE_REALTTIMEDATABSE_URL
-if ( !databaseURL ) {
-    throw new Error("undefined")
-}
-
-const app = firebase.initializeApp({
-    databaseURL: databaseURL,
-})
-
-export const databse = getDatabase(app)
---
+++ src/lib/server/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeServerApp } from 'firebase/app'
+import { get, getDatabase, ref } from 'firebase/database'
+
+const databaseURL = process.env.FIREBASE_REALTTIMEDATABSE_URL
+if ( !databaseURL ) {
+    throw new Error("undefined")
+}
+
+const app = initializeServerApp({
+    databaseURL: databaseURL,
+}, {
+    
+})
+
+const database = getDatabase(app)
+
+const findName = async (name: string): Promise<any> => {
+    const snapshot = await get(ref(database, "tickers/" + name))
+    return snapshot.val()
+}
+
+export { findName }
