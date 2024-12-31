4be9e00691d239b1ee7c71bf7ca7222c26b2d7a3
f6o <25875630+f6o@users.noreply.github.com>
2024-12-31 10:35:28 +0900

    fix

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeServerApp } from 'firebase/app'
 import { get, getDatabase, ref } from 'firebase/database'
 
-const databaseURL = process.env.FIREBASE_REALTTIMEDATABSE_URL
+const databaseURL = process.env.FIREBASE_REALTIMEDATABASE_URL;
 if ( !databaseURL ) {
     throw new Error("undefined")
 }
@@ -10,9 +10,9 @@ const app = initializeServerApp({
     databaseURL: databaseURL,
 }, {
     
-})
+});
 
-const database = getDatabase(app)
+const database = getDatabase(app);
 
 const findName = async (name: string): Promise<any> => {
     const snapshot = await get(ref(database, "tickers/" + name))
