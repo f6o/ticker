c790bb79f1a49d26162539bb6719a94bb1638a1c
f6o <25875630+f6o@users.noreply.github.com>
2025-01-03 06:49:23 +0900

    use environment value instead of token json file

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,9 +1,13 @@
-import { FIREBASE_REALTIMEDATABASE_URL } from '$env/static/private';
+import { FIREBASE_PROJECT_CLIENT_EMAIL, FIREBASE_PROJECT_ID, FIREBASE_REALTIMEDATABASE_URL } from '$env/static/private';
 import admin from 'firebase-admin';
-import { applicationDefault } from 'firebase-admin/app';
+import { cert } from 'firebase-admin/app';
 
 const app = admin.initializeApp({
-    credential: applicationDefault(),
+    credential: cert({
+        projectId: FIREBASE_PROJECT_ID,
+        clientEmail: FIREBASE_PROJECT_CLIENT_EMAIL,
+        privateKey: FIREBASE_PROJECT_PRIVATE_KEY,
+    }),
     databaseURL: FIREBASE_REALTIMEDATABASE_URL,
 });
 
