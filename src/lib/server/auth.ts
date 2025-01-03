331d7baa7260019503d656fc975c07f5a178a28b
f6o <25875630+f6o@users.noreply.github.com>
2025-01-03 17:06:22 +0900

    fix

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -7,9 +7,6 @@ const salt = AUTH_SALT || 'saltysalt';
 
 export const getAuthToken = (name: string, passphrase: string) => {
     const body = `${name}/${passphrase}`;
-    console.log(salt);
-    console.log(body);
-
     const hash = createHmac('sha1', salt)
         .update(body)
         .digest('hex');
