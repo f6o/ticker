0cbdf13b478f37ed9df448a93566cd88315a1dda
f6o <25875630+f6o@users.noreply.github.com>
2025-01-07 21:47:47 +0900

    pretty good

--- src/routes/tickers/[slug]/manage/+page.svelte
+++ src/routes/tickers/[slug]/manage/+page.svelte
@@ -24,11 +24,6 @@
     let textColor = $state('');
     let tickerColor = $state('');
 
-    function resetWins() {
-        player1Wins = 0;
-        player2Wins = 0;
-    }
-
     if ( data.isAuthenticated ) {
         const ref = getTickerRef(data.slug);
 
@@ -57,7 +52,9 @@
 <div class="container">
 {#if data.isAuthenticated}
 <h1>設定画面</h1>
-<h2>情報</h2>
+<details open>
+<!-- svelte-ignore a11y_no_redundant_roles -->
+<summary role="button" class="secondary">戦績</summary>
 {#if loading}
 <div>
     <p aria-busy="true">情報取得中...</p>
@@ -74,39 +71,36 @@
 <div class="grid">
     <div>
         <!-- svelte-ignore a11y_no_redundant_roles -->
+        <label>1P側
         <fieldset role="group">
             <input name="player1" id="Lplayer1" bind:value={player1Name} />
             <input name="player1wins" type="number" bind:value={player1Wins} />
         </fieldset>
+        </label>
     </div>
     <div>
+        <label>中央のテキスト
         <input name="centerText" id="LcenterText" bind:value={centerText} />
+        </label>
     </div>
     <div>
+        <label>2P側
         <!-- svelte-ignore a11y_no_redundant_roles -->
-        <fieldset role="group">
+        <fieldset role="group">            
             <input name="player2" id="Lplayer2" bind:value={player2Name} />
             <input name="player2wins" type="number" bind:value={player2Wins} />
         </fieldset>
+        </label>
     </div>
 </div>
-<div class="grid">
-    <div>
-        <!-- filler -->
-    </div>
-    <div>
-        <button type="button" class="secondary" onclick={resetWins}>勝利数リセット</button>
-    </div>
-    <div>
-        <button type="submit">更新</button>
-    </div>
-</div>
+<button type="submit">更新</button>
 </form>
 {/if}
-<hgroup>
-<h2>レイアウト情報</h2>
+</details>
+<details>
+<!-- svelte-ignore a11y_no_redundant_roles -->
+<summary role="button" class="secondary">色設定</summary>
 <p class="caution">自動で反映されないため、色の設定後、OBSのブラウザソースのプロパティ画面から「現在のページのキャッシュを更新」をしてください。</p>
-</hgroup>
 {#if loading}
 <div>
     <p aria-busy="true">情報取得中...</p>
@@ -121,24 +115,25 @@
 }}>
     <input type="hidden" name="name" value="{data.slug}" />
     <label>
-    背景色
+    背景色 <code>{backgroundColor}</code>
     <input type="color" name="backgroundColor" bind:value={backgroundColor} />
     </label>
     <label>
-    プレイヤー名の文字色
+    プレイヤー名の文字色 <code>{foregroundColor}</code>
     <input type="color" name="foregroundColor" bind:value={foregroundColor} />
     </label>
     <label>
-    勝利数/中央の文字色
+    勝利数/中央の文字色 <code>{textColor}</code>
     <input type="color" name="textColor" bind:value={textColor} />
     </label>
     <label>
-    中央の背景色
+    中央の背景色 <code>{tickerColor}</code>
     <input type="color" name="tickerColor" bind:value={tickerColor} />
     </label>
     <button type="submit">更新</button>
 </form>
 {/if}
+</details>
 {:else}
 <h1>パスフレーズを入力してください</h1>
 <form method="POST" action="/tickers?/auth">
